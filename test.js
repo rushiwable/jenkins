5486e3cd2b1e434f3d683c12b5c677a0d3e92211
Rishi's Codex <104495030+rushiwable@users.noreply.github.com>
2024-01-17 14:10:37 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var request = require('supertest');
 var app = require('../index.js');
 describe('GET /', function() {
- it('respond with hello Rushikesh', function(done) { //navigate to root and check the the response is "hello world"
+ it('respond with hello Rushikesh ', function(done) { //navigate to root and check the the response is "hello world"
  request(app).get('/').expect('hello world', done);
  });
 });
