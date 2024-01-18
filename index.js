7695e18cbf574d3dbef6c8075b91ed9987f37c16
devops <devops@gmail.com>
2024-01-18 10:35:46 +0000

    1234

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('I am ABHI & I am fan of msd07');
+ res.send('I Rishi');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
